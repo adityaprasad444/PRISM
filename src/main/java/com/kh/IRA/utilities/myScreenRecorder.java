c670bd40119f722014a1373b6d8123184efdd007
AdityaPrasad <aditya.prasad@knowledgehut.co>
2020-11-09 22:02:34 +0530

    recording changed to AVI

--- src/main/java/com/kh/IRA/utilities/myScreenRecorder.java
+++ src/main/java/com/kh/IRA/utilities/myScreenRecorder.java
@@ -60,7 +60,7 @@ public class myScreenRecorder extends ScreenRecorder {
 				.getDefaultConfiguration();
 
 		screenRecorder = new myScreenRecorder(gc, captureSize,
-				new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_MP4),
+				new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
 				new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
 						CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, DepthKey, 24, FrameRateKey,
 						Rational.valueOf(15), QualityKey, 1.0f, KeyFrameIntervalKey, 15 * 60),
